6f90aa2fbcddcbf6b947d2093cbd81cd4ae204c3
kurniawan7-k <wawanhendrawan869@gmail.com>
2023-12-13 22:24:18 +0700

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="mb-0 text-center">
+        <div className="d-flex align-items-center justify-content-center pb-5">
+          <div className="col-md-6">
+            <p className="mb-3 mb-md-0">Ciwak Leeoy
+            </p>
+            <a className="text-dark fs-4" href="https://github.com/kurniawan7-k" target="_blank" rel="noreferrer">
+              <i className="fa fa-github"></i>
+            </a>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
